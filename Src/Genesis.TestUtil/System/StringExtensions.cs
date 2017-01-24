f5cee8ee313ad1abecc7fc8885f295d39787bc0c
Kent Boogaart <kent.boogaart@gmail.com>
2017-01-24 12:31:43 +1030

    Add supported formats

--- Src/Genesis.TestUtil/System/StringExtensions.cs
+++ Src/Genesis.TestUtil/System/StringExtensions.cs
@@ -6,12 +6,26 @@
     {
         private static readonly string[] supportedDateTimeFormats = new[]
         {
+            "yyyy-MM-dd HH:mm:ss.fffffff",
+            "yyyy-MM-dd HH:mm:ss.ffffff",
+            "yyyy-MM-dd HH:mm:ss.fffff",
+            "yyyy-MM-dd HH:mm:ss.ffff",
+            "yyyy-MM-dd HH:mm:ss.fff",
+            "yyyy-MM-dd HH:mm:ss.ff",
+            "yyyy-MM-dd HH:mm:ss.f",
             "yyyy-MM-dd HH:mm:ss",
             "yyyy-MM-dd"
         };
 
         private static readonly string[] supportedTimeSpanFormats = new[]
         {
+            @"d\.hh\:mm\:ss\.fffffff",
+            @"d\.hh\:mm\:ss\.ffffff",
+            @"d\.hh\:mm\:ss\.fffff",
+            @"d\.hh\:mm\:ss\.ffff",
+            @"d\.hh\:mm\:ss\.fff",
+            @"d\.hh\:mm\:ss\.ff",
+            @"d\.hh\:mm\:ss\.f",
             @"d\.hh\:mm\:ss",
             @"hh\:mm\:ss",
             @"hh\:mm"
