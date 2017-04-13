38aeb018bc7d08d1941f16abc7d5f0bc9aad3cc4
Kent Boogaart <kent.boogaart@gmail.com>
2017-04-13 10:23:23 +0930

    Migrate to netstandard

--- Src/Genesis.TestUtil/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Genesis.TestUtil")]
-[assembly: AssemblyDescription("Contains the implementation of Genesis.TestUtil.")]
-[assembly: CLSCompliantAttribute(true)]
